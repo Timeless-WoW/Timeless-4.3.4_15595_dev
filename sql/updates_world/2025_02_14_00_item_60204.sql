507d02f338066bb33b4c82ceb9d90dafa4a57d8b
Lifak <lifakofficial@gmail.com>
2025-02-14 21:23:45 -0500

    Create 2025_02_14_00_item_60204.sql

---
+++ sql/updates_world/2025_02_14_00_item_60204.sql
@@ -0,0 +1,6 @@
+/* https://cata-twinhead.twinstar.cz/?item=60204
+   https://cata-twinhead.twinstar.cz/?npc=45785
+*/
+
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -100 WHERE `entry` = 45785 AND `item` = 60204;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -100 WHERE `entry` = 43732 AND `item` = 60204;
