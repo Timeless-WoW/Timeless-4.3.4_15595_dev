10c358cf64e27821b90e62b470c4f8bef1493882
Lifak <lifakofficial@gmail.com>
2025-02-15 16:43:12 -0500

    DB:Item_58111

---
+++ sql/updates_world/2025_02_15_00_quest_26286.sql
@@ -0,0 +1,4 @@
+/*
+https://cata-twinhead.twinstar.cz/?item=58111
+*/
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -25 WHERE `entry` IN (124,452,501,54371,54372,54373) AND `item` = 58111;
