5d64d186c0a87e511a5bf60dc6fda20c3ac011ee
MiSu <mariodanny91@gmail.com>
2025-02-18 18:01:28 +0100

    DB: Misc report_quest

--- sql/updates_world/2025_02_18_00_clean_DB.sql
+++ sql/updates_world/2025_02_18_00_clean_DB.sql
@@ -1,3 +1,18 @@
 /*Clean unused table*/
 
 DROP TABLE report_quest;
+
+
+-- ----------------------------
+-- Table structure for report_quest
+-- ----------------------------
+DROP TABLE IF EXISTS `report_quest`;
+CREATE TABLE `report_quest`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `status` int UNSIGNED NOT NULL,
+  `count` int UNSIGNED NOT NULL,
+  `comment` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1006005 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+SET FOREIGN_KEY_CHECKS = 1;
